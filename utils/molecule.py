e30a9de8ca4af25dd2d8a83623b36678f930c25f
vsc10100 <vsc10100@login1.tier1.hpc.kuleuven.be>
2019-02-11 13:37:34 +0100

    back conversion smiles to index

--- utils/molecule.py
+++ utils/molecule.py
@@ -380,3 +380,4 @@ if __name__=="__main__":
                 print indtosmi(index)
     except IOError:
         print indtosmi(sys.argv[1])
+        print "".join([replacements.get(c, c) for c in sys.argv[1]])
